93bf1b96691affdf61ec0fbdd671fc71e1a88f68
Abhay@0215 <abhayshaw323@gmail.com>
2026-01-12 22:24:07 +0530

    Add Binary Search implementation

---
+++ DSA/Binary Search/BinarySearch.java
@@ -0,0 +1,24 @@
+public class BinarySearch {
+    public static int solution(int[] arr , int target){
+        int n = arr.length;
+        int low = 0, high = n-1;
+        while(low<=high){
+            int mid = low + (high - low)/2;
+            if(arr[mid] == target) {
+                return mid;
+            }
+            else if (arr[mid] < target) { 
+                low = mid + 1;
+            }
+            else { 
+                high = mid - 1 ;
+            }
+        }
+        return -1;
+    }
+    public static void main (String[] args){
+        int arr[] ={-1,0,3,5,9,12};
+        int target = 9;
+        System.out.println(solution(arr,target));
+    }
+}
