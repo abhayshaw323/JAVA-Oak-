121518eed5b95701fbd996cfabbba953c2d0a1e8
Abhay@0215 <abhayshaw323@gmail.com>
2026-02-10 18:40:13 +0530

    intial commit

---
+++ DSA/Binary Search/SingleAmongDouble.java
@@ -0,0 +1,27 @@
+public class SingleAmongDouble {
+    public static int singles(int[] arr){
+        int n = arr.length;
+        if(n == 1) return arr[0];
+        if(arr[0] != arr[1]) return arr[0];
+        if(arr[n-1] != arr[n-2]) return arr[n-1];
+        int lo = 0, hi = n-1;
+        while(lo <= hi){
+            int mid = lo + (hi- lo)/2;
+            if(arr[mid] != arr[mid-1] && arr[mid] != arr[mid+1]) return arr[mid];
+            int f = mid , s= mid;
+            if (arr[mid -1] == arr[mid]) f = mid-1 ;
+            else s = mid + 1;
+            int leftcount = f -lo;
+            int rightcount = hi -s;
+            if(leftcount % 2 == 0) lo = s+1;
+            else hi = f-1;
+
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,1,2,2,3,3,4,50,50,65,65};
+        int x = singles(arr);
+        System.out.println(x);
+    }
+}
