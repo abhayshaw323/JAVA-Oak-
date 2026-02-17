323bc9f73a783ad232a562166f4714956df5b516
Abhay@0215 <abhayshaw323@gmail.com>
2026-02-17 18:29:14 +0530

    initial commit

---
+++ DSA/Recursion/FindElementInArray.java
@@ -0,0 +1,16 @@
+public class FindElementInArray {
+    public static boolean exists (int[] arr, int target, int idx){
+        if(idx == arr.length) return false;
+        if(arr[idx] == target) return true;
+        return exists(arr,target,idx+1);
+        
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {5,3,7,12,76,9,34,5};
+        int target = 76;
+        System.out.println(exists(arr,target,0));
+    }
+
+   
+}
---
+++ DSA/Recursion/powerSet.java
@@ -0,0 +1,15 @@
+public class powerSet {
+    public static void main(String[] args) {
+        String s = "abc";
+        subsets("",s,0);
+    }
+    public static void subsets(String ans, String s, int idx){
+        if(idx==s.length()){
+            System.out.print(ans+" ");
+            return;
+        }
+        char ch = s.charAt(idx);
+        subsets(ans+ch,s,idx+1);
+        subsets(ans,s,idx+1);
+    }
+}
