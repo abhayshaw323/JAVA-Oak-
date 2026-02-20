d5bc710fb23a871c2423f2c8a801a1050313661b
Abhay@0215 <abhayshaw323@gmail.com>
2026-02-20 17:52:11 +0530

    initial commit

---
+++ DSA/Recursion/PreinPost.java
@@ -0,0 +1,13 @@
+public class PreinPost {
+    public static void main(String[] args) {
+        pip(3);
+    }
+    public static void pip(int n){
+        if(n==0) return;
+        System.out.println(n+" "); //pre
+        pip(n-1);
+        System.out.println(n+" "); // in
+        pip(n-1);
+        System.out.println(n+" "); // post
+    }
+}
