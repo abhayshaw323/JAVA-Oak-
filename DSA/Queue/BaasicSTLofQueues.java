35e1cb56031ce1fd2c182f3b84557bb6b488bec6
Abhay@0215 <abhayshaw323@gmail.com>
2026-05-10 10:47:47 +0530

    Added queue folder

---
+++ DSA/Queue/BaasicSTLofQueues.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class BaasicSTLofQueues {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        q.add(40);
+        System.out.println(q+" "+q.peek());
+        q.remove();
+        System.out.println(q+ " "+q.size());
+    }
+}
